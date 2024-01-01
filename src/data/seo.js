95a3fc1793cbef8a618ed34769052148d42b0d4c
Bhushan Kadam <79011807+Bhushankadam17@users.noreply.github.com>
2024-01-01 11:31:35 +0530

    seacond commit

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a full stack developer with expertise in creating amazing UI powered with strong backend. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies for my clients. I enjoy solving complex problems and learning new skills. I am always looking for new challenges and opportunities to grow as a developer",
+		keywords: ["bhushan", "bhushan kadam", "bhushan k"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a full stack developer with expertise in creating amazing UI powered with strong backend. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies for my clients. I enjoy solving complex problems and learning new skills. I am always looking for new challenges and opportunities to grow as a developer",
+		keywords: ["bhushan", "bhushan kadam", "bhushan k"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"I've worked on a variety of projects for my clients and I'm proud of the change I've made. I am trying to live a life where I can follow both of my passions together coding and travelling. I have made many projects for my clients and I have learnt a lot from them. If you wanna make your business grow online I am happy to help ",
+		keywords: ["bhushan", "bhushan kadam", "bhushan k"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects for my clients and I'm proud of the change I've made. I am trying to live a life where I can follow both of my passions together coding and travelling. I have made many projects for my clients and I have learnt a lot from them. If you wanna make your business grow online I am happy to help ",
+		keywords: ["bhushan", "bhushan kadam", "bhushan k"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in growing your business online, feel free to reach out to me. I'm always excited for new challenges and opportunities.",
+		keywords: ["bhushan", "bhushan kadam", "bhushan k"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,75 @@
+const INFO = {
+	main: {
+		title: "Bhushan Kadam",
+		name: "Bhushan Kadam",
+		email: "me@developedbybhushan.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/BhushanKadam011",
+		github: "https://github.com/Bhushankadam17",
+		linkedin: "https://www.linkedin.com/in/bhushan-kadam011/",
+		instagram: "https://www.instagram.com/_bhushan_kadam_/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer, I like to travel and play chess.",
+		description:
+			"I am a full stack developer with expertise in creating amazing UI powered with strong backend. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies for my clients. I enjoy solving complex problems and learning new skills. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Bhushan Kadam. I live in imagination and i work remotely from there.",
+		description:
+			"I've worked on a variety of projects for my clients and I'm proud of the change I've made. I am trying to live a life where I can follow both of my passions together coding and travelling. I have made many projects for my clients and I have learnt a lot from them. If you wanna make your business grow online I am happy to help 😊",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Ambre Holidays",
+			description:
+				"My first freelance project was for a travel agency and I think i did pretty good job but I wanted a challenge so I made it in pure JS.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://ambreholidays.com/",
+		},
+
+		{
+			title: "Sushshree HR Solutions",
+			description:
+				"I made this project to in NEXT and I learned a lot while making it also faced challenger but in the end I did it.",
+			logo: "../next.png",
+			linkText: "View Project",
+			link: "https://sushshree.in/",
+		},
+
+		{
+			title: "Bharat Bus",
+			description:
+				"As a co founder and tech lead I worked on this project alone for version 1 and later made a team to upgrade this project to version 2",
+			logo: "../react.png",
+			linkText: "View Project",
+			link: "https://bharatbus.site/",
+		},
+
+		// {
+		// 	title: "Teachsistance",
+		// 	description:
+		// 		"Automatic timetable generator using generic algorithms. Finale year project I used mostly react and django for website but hardest part was the algorithm.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+
+
+	],
+};
+
+export default INFO;
